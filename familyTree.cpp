b576dccd772fcea5854469dde3f938eafc7b86bf
Demokrit5 <52154445+Demokrit5@users.noreply.github.com>
2019-06-24 12:24:31 +0200

    Update familyTree.cpp

--- familyTree.cpp
+++ familyTree.cpp
@@ -32,33 +32,33 @@ void Tree::printChildren(const int gid)
     std::vector<int> c_gids = person->getChildren();
     if(!c_gids.empty())
     {
-        std::cout <<person->getName() +" hat die Kinder: "<<std::endl;
         for(unsigned int i = 0; i < c_gids.size(); ++i)
         {
             Person* p = getPerson(c_gids[i]);
-            std::cout <<p->getName() << std::endl;
+            std::cout << p->getName() << std::endl;
         }
     }
     else
     {
-        std::cout <<  person->getName() + " hat keine Kinder" << std::endl;
+        Person* p = getPerson(gid);
+        std::cout << p->getName() + " hat keine Kinder" << std::endl;
     }
+    
     return;
 }
 
 void Tree::printParents(const int gid)
 {
     Person* person = getPerson(gid);
-    std::cout <<person->getName() +" hat die Eltern: "<<std::endl;
     if(person->getPa()!= -1)
     {
         Person* p = getPerson(person->getPa());
-        std::cout <<p->getName() << std::endl;
+        std::cout << p->getName() << std::endl;
     }
     if(person->getPb()!= -1)
     {
         Person* p = getPerson(person->getPb());
-        std::cout <<p->getName() << std::endl;
+        std::cout << p->getName() << std::endl;
     }
     if((person->getPa() == -1)&&(person->getPb() == -1))
     {
@@ -80,23 +80,38 @@ void Tree::printSiblings(const int gid)
     {
         siblings = getPerson(person->getPb())->getChildren();
     }
-
-
+    
     //Ausgabe
     if(siblings.size() >= 2)
     {
         int anzahl = siblings.size()-1;   //-1 weil die Person selbst ja auch ein Kind ist
         std::cout << person->getName() << " hat " << anzahl << " Geschwister:" << std::endl;
-        std::cout <<person->getName() +" hat die Geschwister: "<<std::endl;
         for(unsigned int i = 0; i<siblings.size(); ++i)
-        {
+        {           
             if (getPerson(siblings[i]) != person)
-                std::cout <<getPerson(siblings[i])->getName() << std::endl;
+                std::cout << getPerson(siblings[i])->getName() << std::endl;
         }
     }
     else
     {
         std::cout << person->getName() << " hat keine Geschwister oder sie sind unbekannt." << std::endl;
+    }
+    return;
+}
+
+void Tree::printGrandparents(const int gid)
+{
+    Person* person = getPerson(gid);
+    if(person->getPa() != -1 || person->getPb() != -1)
+    {
+        if(person->getPa() != -1)
+        {
+            printParents(person->getPa());
+        }
+        if(person->getPb() != -1)
+        {
+            printParents(person->getPb());
+        }               
     }
     return;
 }
